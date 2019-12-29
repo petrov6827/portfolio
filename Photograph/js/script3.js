983c4152a0148efa73f7f1a6e8862ed2685c1097
petrov6827 <37485683+petrov6827@users.noreply.github.com>
2019-12-30 00:02:25 +0300

    Update script3.js

--- Photograph/js/script3.js
+++ Photograph/js/script3.js
@@ -2,17 +2,22 @@ const name = document.getElementById('name')
 const email = document.getElementById('email')
 const form = document.getElementById('form')
 const errorElement = document.getElementById('error')
+const popup = document.getElementById('popup')
 
 form.addEventListener('submit', (e) => {
     let messages = []
     if (name.value === '' || name.value == null) {
-        messages.push('Name is required')
+        messages.push('Name is required');
+        popup.style.display = 'none';
+        c = 0;
     }
-
+    if (email.value === '' || email.value == null) {
+        messages.push('Email is required');
+        popup.style.display = 'none';
+        c = 0;
+    }
     if (messages.length > 0) {
         e.preventDefault()
-        errorElement.innerText = messages.join(' ,')
+        errorElement.innerText = messages.join(', ')
     }
 })
-
-
