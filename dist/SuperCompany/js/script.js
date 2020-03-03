b4bb72350b5f110bd62e45363c9d495ae83bf94c
Dmitry <petrov6827@gmail.com>
2020-03-03 17:47:34 +0300

    Add testCotober, TestFigma

---
+++ dist/SuperCompany/js/script.js
@@ -0,0 +1,1 @@
+$('.banner__slider').slick();
--- dist/js/script.js
+++ dist/js/script.js
@@ -1,36 +1,31 @@
-var Toggle = document.querySelector('.main-nav__toggle');
-var Close = document.querySelector('.main-nav__close');
-var Menu = document.querySelector('.menu');
-var Triangle = document.querySelector('.main__triangle');
+// var Toggle = document.querySelector('.main-nav__toggle');
+// var Close = document.querySelector('.main-nav__close');
+// var Menu = document.querySelector('.menu');
+// var Triangle = document.querySelector('.main__triangle');
 
-Toggle.addEventListener('click',function(event) {
-    event.preventDefault();
-    Menu.classList.add('menu--show');
-    Toggle.classList.add('toggle__hidden');
-    Triangle.classList.add('main__triangle--close');
+// Toggle.addEventListener('click', function(event) {
+//   event.preventDefault();
+//   Menu.classList.add('menu--show');
+//   Toggle.classList.add('toggle__hidden');
+//   Triangle.classList.add('main__triangle--close');
+// });
+
+// Close.addEventListener('click', function(event) {
+//   event.preventDefault();
+//   Menu.classList.remove('menu--show');
+//   Toggle.classList.remove('toggle__hidden');
+//   Triangle.classList.remove('main__triangle--close');
+// });
+
+let Item = document.querySelector('.item');
+let About = document.querySelector('.item__about');
+
+Item.addEventListener('mouseover', function(event) {
+  event.preventDefault();
+  About.classList.add('.item__about--visible');
 });
 
-Close.addEventListener('click',function(event) {
-    event.preventDefault();
-    Menu.classList.remove('menu--show');
-    Toggle.classList.remove('toggle__hidden');
-    Triangle.classList.remove('main__triangle--close');
+Item.addEventListener('mouseout', function(event) {
+  event.preventDefault();
+  About.classList.remove('.item__about--visible');
 });
-
-
-
-function sayHiLater() {
-  
-  var greeting = 'Hi!';
-
-  setTimeout(function() {
-
-        console.log(greeting);
-
-  }, 1000);
-
-}
-
-sayHiLater();
-
-
