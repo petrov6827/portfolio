795460d09dcc8e3dfff2a6a43bd8c58297921337
Dmitrii Petrov <n4ela2015@yandex.ru>
2024-03-28 14:02:26 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -1,18 +1,31 @@
-function sayHiLater() {
-  var greeting = 'Привет заказчик! =)';
+// var Toggle = document.querySelector('.main-nav__toggle');
+// var Close = document.querySelector('.main-nav__close');
+// var Menu = document.querySelector('.menu');
+// var Triangle = document.querySelector('.main__triangle');
 
-  setTimeout(function() {
-    console.log(greeting);
-  }, 1000);
-}
+// Toggle.addEventListener('click', function(event) {
+//   event.preventDefault();
+//   Menu.classList.add('menu--show');
+//   Toggle.classList.add('toggle__hidden');
+//   Triangle.classList.add('main__triangle--close');
+// });
 
-sayHiLater();
+// Close.addEventListener('click', function(event) {
+//   event.preventDefault();
+//   Menu.classList.remove('menu--show');
+//   Toggle.classList.remove('toggle__hidden');
+//   Triangle.classList.remove('main__triangle--close');
+// });
 
-function myFunction() {
-  var x = document.getElementById('myTopnav');
-  if (x.className === 'topnav') {
-    x.className += ' responsive';
-  } else {
-    x.className = 'topnav';
-  }
-}
+let Item = document.querySelector('.item');
+let About = document.querySelector('.item__about');
+
+Item.addEventListener('mouseover', function(event) {
+  event.preventDefault();
+  About.classList.add('.item__about--visible');
+});
+
+Item.addEventListener('mouseout', function(event) {
+  event.preventDefault();
+  About.classList.remove('.item__about--visible');
+});
